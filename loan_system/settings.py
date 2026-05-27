67b2b235b0f5a2d745b680e980bee40c62479fd7
Manoj Yadav K N <mayayadav02012002@gmail.com>
2026-05-27 12:36:50 +0530

    Changed database to SQLite

--- loan_system/settings.py
+++ loan_system/settings.py
@@ -55,15 +55,8 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'loan_prediction_db',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
