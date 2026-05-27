85f24faac0de9f5cf85de7659bc323add4b363aa
Manoj Yadav K N <mayayadav02012002@gmail.com>
2026-05-27 13:01:26 +0530

    Fixed ALLOWED_HOSTS

--- loan_system/settings.py
+++ loan_system/settings.py
@@ -7,7 +7,11 @@ SECRET_KEY = 'your-secret-key-here'  # Change in production
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'loan-prediction-system-gfcf.onrender.com',
+    '127.0.0.1',
+    'localhost',
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
